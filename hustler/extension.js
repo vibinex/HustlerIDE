8eca8b0992227731042982260493b9bd6128ea0b
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2024-04-02 12:28:56 +0530

    Added a prompt to nudge the developer to talk to his users (default interval set to 1 hour).

--- hustler/extension.js
+++ hustler/extension.js
@@ -2,26 +2,46 @@
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+let intervalId;
 
 /**
  * @param {vscode.ExtensionContext} context
- */
+*/
+// Inside extension.js
 function activate(context) {
+	const INTERVAL_DURATION = 60 * 60 * 1000;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "hustler" is now active!');
+	intervalId = setInterval(() => {
+		vscode.commands.executeCommand('hustler.growthPrompt');
+	}, INTERVAL_DURATION);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('hustler.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+	let disposable = vscode.commands.registerCommand('hustler.growthPrompt', async function () {
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from HustlerIDE!');
+		const yesItem = { title: 'Yes' };
+		const notYetItem = { title: 'Not yet' };
+		const notNeededItem = { title: 'Not needed for this project' };
+
+		const selection = await vscode.window.showInformationMessage(
+			'Hey, have you spent any time talking to customers or marketing this product today?',
+			yesItem, notYetItem, notNeededItem
+		);
+
+		if (selection === yesItem) {
+			clearInterval(intervalId);
+			setTimeout(() => {
+				intervalId = setInterval(() => {
+					vscode.commands.executeCommand('hustler.growthPrompt');
+				}, INTERVAL_DURATION);
+			}, new Date().setHours(24, 0, 0, 0) - new Date());
+
+		} else if (selection === notYetItem) {
+			// do nothing
+
+		} else if (selection === notNeededItem) {
+			clearInterval(intervalId);
+			intervalId = null;
+		}
+
 	});
 
 	context.subscriptions.push(disposable);
